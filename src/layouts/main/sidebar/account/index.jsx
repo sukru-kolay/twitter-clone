0ae2a8bb9e8b933b9ffca1d63af82efb834d7de5
sukrukolay <sukru.kolay1907@gmail.com>
2023-10-30 22:09:02 +0300

    hooks

--- src/layouts/main/sidebar/account/index.jsx
+++ src/layouts/main/sidebar/account/index.jsx
@@ -1,10 +1,21 @@
 import { Popover } from "@headlessui/react";
+import { useAccount } from "../../../../store/auth/hooks";
 export const Account = () => {
+  const account = useAccount();
   return (
     <div className="mt-auto">
       <Popover>
         <Popover.Button className="my-3 p-3 rounded-full  w-[89%] bg-[red]">
-          <div>test</div>
+          <div className="flex align-center">
+            <div>
+              <img
+                src={account.avatar}
+                alt="Logo"
+                className="w-10 h-10 rounded-full"
+              />
+            </div>
+            <div className="m-3">{account.username}</div>
+          </div>
         </Popover.Button>
         <Popover.Panel>açılacak</Popover.Panel>
       </Popover>
