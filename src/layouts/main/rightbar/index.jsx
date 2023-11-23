36c8ed98b375c36e8233b846091cac9a4572e500
KOLAY <kolay@KOLAY-MacBook-Air.local>
2023-11-23 21:34:44 +0300

    abone ol

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Search from "./search";
+import Premium from "../../../components/premium";
 const RightBar = () => {
-  return <aside className="w-[350px] mr-2.5"><Search/></aside>;
+  return( 
+  <aside className="w-[350px] mr-2.5">
+    <Search/>
+    <Premium/>
+  </aside>
+  )
 };
 
 export default RightBar;
